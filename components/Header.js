9f8149f21fb584b79997784e796b579a8e3e1751
Pemba Sherpa <pembasherpa@pembas-MacBook-Pro.local>
2021-08-07 12:15:24 +0545

    changes ok

--- components/Header.js
+++ components/Header.js
@@ -15,8 +15,8 @@ function Header() {
             </div>
 
             {/* Middle Heading */}
-            <div className="flex items-center border-2 rounded-full py-2 md:shadow-sm px-4">
-                <input type="text" placeholder="Search" className="bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400 "/>
+            <div className="flex items-center border-2 rounded-full py-2 md:shadow-sm">
+                <input type="text" placeholder="Search" className="bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400 px-4 "/>
                 <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2 "/>
             </div>
 
