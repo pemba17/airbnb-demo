3e7778eea3cf89f25da795e45cc4c96681ff62de
Pemba Sherpa <pembasherpa@pembas-MacBook-Pro.local>
2021-08-07 11:23:41 +0545

    changes

--- components/Banner.js
+++ components/Banner.js
@@ -3,9 +3,9 @@ function Banner() {
     return (
         <div className="relative h-[300px] sm:h-[400px[ md:h-[500px] lg:h-[600px] 2xl:h-[700px]">
            <Image src="https://shorturl.at/pACD5" layout="fill" objectFit="cover"/>
-           <div className="absolute top-1/4 w-1/2 lg:w-1/4 left-6">
+           <div className="absolute top-1/4 w-1/2 lg:w-1/4 left-6 lg:left-28">
                <p className="text-2xl lg:text-6xl text-extrabold text-white">Olympian & Paralympian Online Experiences</p>
-               <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full my-8 hover:shadow-xl active:scale-90 transition duration-150 font-bold ">Im Flexible</button>
+               <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full my-8 hover:shadow-xl active:scale-90 transition duration-150 font-bold ">Explore More</button>
            </div>
         </div>
     )
