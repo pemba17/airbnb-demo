727f145d35ab0eae0cb54652ac7426523d029715
Pemba Sherpa <pembasherpa@pembas-MacBook-Pro.local>
2021-08-11 11:35:05 +0545

    changes

--- components/Map.js
+++ components/Map.js
@@ -1,6 +1,8 @@
 import ReactMapGL,{Marker,Popup} from 'react-map-gl'
 import {useState} from "react"
 import getCenter  from 'geolib/es/getCenter';
+import Image from "next/image"
+import { StarIcon } from "@heroicons/react/solid";
 function Map({searchResults}) {
 
     const [selectedLocation,setSelectedLocation]=useState({});
@@ -32,7 +34,21 @@ function Map({searchResults}) {
                     <Marker longitude={result.long} latitude={result.lat} offsetLeft={-20} offsetTop={-10}><p onClick={()=>setSelectedLocation(result)} className="text-2xl animate-bounce cursor-pointer">📌</p></Marker>
 
                     {selectedLocation.long===result.long?(
-                        <Popup closeOnClick={true} onClose={()=>setSelectedLocation({})} longitude={result.long} latitude={result.lat} >{result.title}</Popup>
+                        <Popup closeOnClick={true} onClose={()=>setSelectedLocation({})} longitude={result.long} latitude={result.lat} className="w-72 cursor-pointer"><div className="relative h-40 w-[265px] hover:opacity-80">
+                            <Image src={result.img} layout="fill" objectFit="cover"/>
+                            </div>
+                            <div className="flex flex-col mt-2">
+                                <div className="flex justify-between items-center">
+                                    <p className="text-sm">{result.title}</p>
+                                    <p className="text-sm">{result.price}</p>
+                                </div>
+
+                                <div className="flex items-center pt-2">
+                                    <p><StarIcon className="h-5 text-red-400"/></p>
+                                    <p className="text-sm">{result.star}</p>
+                                </div>
+                            </div>    
+                        </Popup>
                     ):(
                         false
                     )}
